16a48fbf0bc874cc7cc86378923ecb4f7f38bff9
Ashimi Kathriarachchi <ashimikath@hotmail.com>
2019-09-19 22:11:13 +0530

    create super user and create data retrive page

--- supplier/models.py
+++ supplier/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class SupplierProductInfo(models.Model):
+    Status = (
+        ('available', 'Available'),
+        ('lessStock', 'Less Amount of Stock'),
+        ('outOfStock', 'Out of Stock')
+    )
+
+    Product_id = models.CharField(max_length=6, blank=False,)
+    Product_Name = models.CharField(max_length=100)
+    Supplier_Name = models.CharField(max_length=100)
+    Qty = models.IntegerField()
+    Amount = models.CharField(max_length=50)
+    Order_date = models.DateTimeField(auto_now=True)
+    Status = models.CharField(max_length=50, choices=Status, default='Available')
+
+    def __str__(self):
+        return 'Product_id: {0} Product_Name:{1} Supplier_Name:{2} Qty:{3}  Amount:{4} Order_date:{5} Status:{6}'.format(
+            self.Product_id, self.Product_Name, self.Supplier_Name, self.Qty, self.Amount, self.Order_date, self.Status)
--- supplier/urls.py
+++ supplier/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 
 urlpatterns =[
 path('',views.supplierhome, name='supplierhomepage'),
+path('display/',views.displaySupplierProducts, name='SupplierProducts'),
 ]
