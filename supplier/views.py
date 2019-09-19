aa50735c2d62e42bb106cf798464d50853eecc16
Ashimi Kathriarachchi <ashimikath@hotmail.com>
2019-09-19 22:18:07 +0530

    Add supplier homepage

--- supplier/views.py
+++ supplier/views.py
@@ -5,10 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def supplierhome(request):
-    return render(request, '')
+    return render(request, 'supplier/supplier_homepage.html')
 
 def displaySupplierProducts(request):
     info = SupplierProductInfo.objects.all()
     context = {'info': info,
                'dashboard_dir': 'SupplierProductInfo'}
-    return render(request, 'supplier/supplier_homepage.html',context)
+    return render(request, 'supplier/supplier_productpage.html',context)
