127efa0131463605ece45714fd79a18306290e2e
Chathurindu <100wpck@gmail.com>
2019-09-25 12:46:15 +0530

    ashmi

--- supplier/urls.py
+++ supplier/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import url
 urlpatterns =[
 path('',views.supplierhome, name='supplierhomepage'),
 path('display/',views.displaySupplierProducts, name='displaySupplierProducts'),
-path('addsupplierinfo/', views.supplierinfoform, name='addsupplierinfo'),
+path('display/addsupplierinfo/', views.supplierinfoform, name='addsupplierinfo'),
 
 url(r'^edit_supplieritem/(?P<pk>\d+)' , views.edit_supplieritem, name='edit_supplieritem'),
+url(r'^delete_supplieritem/(?P<pk>\d+)' , views.delete_supplieritem, name='delete_supplieritem'),
 ]
